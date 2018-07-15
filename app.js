4ca21a5c95284d86cb2585ad452c9e8212f5e633
Sohel Shaikh <rebht78@gmail.com>
2018-07-15 11:38:03 +0530

    added delete, put and post route to app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
 
+app.use(express.json());
 const movies = [
     {id:1,movieName:'Jurassic Park'},
     {id:2,movieName:'The Lost World'},
@@ -21,6 +22,41 @@ app.get('/api/movies/:id', (req,res) => {
         return res.status(404).send('No Movies with specified ID found!');
     }
     res.send(movie);
+});
+
+app.post('/api/movies', (req,res) => {
+    const movie = {
+        id: movies.length + 1,
+        movieName:req.body.movieName
+    };
+
+    movies.push(movie);
+
+    res.send(movies);
+});
+
+app.put('/api/movies/:id', (req,res) => {
+    if (req.params.id >= movies.length)
+    {
+        return res.status(400).send('Movie ID is not valid!');
+    }
+    else
+    {
+        movies[req.params.id - 1].movieName = req.body.movieName;
+    }
+    res.send(movies);
+});
+
+app.delete('/api/movies/:id', (req,res) => {
+    if (req.params.id >= movies.length)
+    {
+        return res.status(400).send('Movie ID is not valid!');
+    }
+    else
+    {
+        movies.splice(req.params.id - 1,1);
+    }
+    res.send(movies);
 });
 
 app.listen(port, () => {
