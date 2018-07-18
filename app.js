f1ace464547524458acc85f9ecca0588fab48346
Sohel Shaikh <rebht78@gmail.com>
2018-07-18 21:10:17 +0530

    added routes folder for movies route

--- app.js
+++ app.js
@@ -1,66 +1,12 @@
 const express = require('express');
+const router = require('./routes/movies');
 const app = express();
 const port = process.env.port || 3000;
 
 //middleware configuratio
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
-const movies = [
-    {id:1,movieName:'Jurassic Park'},
-    {id:2,movieName:'The Lost World'},
-    {id:3,movieName:'The Study in Pink'},
-    {id:4,movieName:'The Blind Banker'}
-];
-
-app.get('/api/movies', (req,res) => {
-    res.send(movies);
-});
-
-app.get('/api/movies/:id', (req,res) => {
-    const movie = movies.find(function(item){
-        return item.id == req.params.id;
-    });
-    if (!movie) {
-        return res.status(404).send('No Movies with specified ID found!');
-    }
-    res.send(movie);
-});
-
-app.post('/api/movies', (req,res) => {
-    const movie = {
-        id: movies.length + 1,
-        movieName:req.body.movieName
-    };
-
-    movies.push(movie);
-
-    res.send(movies);
-});
-
-app.put('/api/movies/:id', (req,res) => {
-    if (req.params.id >= movies.length)
-    {
-        return res.status(400).send('Movie ID is not valid!');
-    }
-    else
-    {
-        movies[req.params.id - 1].movieName = req.body.movieName;
-    }
-    res.send(movies);
-});
-
-app.delete('/api/movies/:id', (req,res) => {
-    if (req.params.id >= movies.length)
-    {
-        return res.status(400).send('Movie ID is not valid!');
-    }
-    else
-    {
-        movies.splice(req.params.id - 1,1);
-    }
-    res.send(movies);
-});
+app.use('/api/movies',router);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}....`);
---
+++ routes/movies.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+
+const movies = [
+    {id:1,movieName:'Jurassic Park'},
+    {id:2,movieName:'The Lost World'},
+    {id:3,movieName:'The Study in Pink'},
+    {id:4,movieName:'The Blind Banker'}
+];
+
+router.get('/', (req,res) => {
+    res.send(movies);
+});
+
+router.get('/:id', (req,res) => {
+    const movie = movies.find(function(item){
+        return item.id == req.params.id;
+    });
+    if (!movie) {
+        return res.status(404).send('No Movies with specified ID found!');
+    }
+    res.send(movie);
+});
+
+router.post('/', (req,res) => {
+    const movie = {
+        id: movies.length + 1,
+        movieName:req.body.movieName
+    };
+
+    movies.push(movie);
+
+    res.send(movies);
+});
+
+router.put('/:id', (req,res) => {
+    if (req.params.id >= movies.length)
+    {
+        return res.status(400).send('Movie ID is not valid!');
+    }
+    else
+    {
+        movies[req.params.id - 1].movieName = req.body.movieName;
+    }
+    res.send(movies);
+});
+
+router.delete('/:id', (req,res) => {
+    if (req.params.id >= movies.length)
+    {
+        return res.status(400).send('Movie ID is not valid!');
+    }
+    else
+    {
+        movies.splice(req.params.id - 1,1);
+    }
+    res.send(movies);
+});
+
+module.exports = router;
