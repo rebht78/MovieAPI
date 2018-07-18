969ab6a444647f962cbe25e7e752b01c5ad49c02
Sohel Shaikh <rebht78@gmail.com>
2018-07-18 21:34:43 +0530

    Added config module to separate production and development environment

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const config = require('config');
 const express = require('express');
 const router = require('./routes/movies');
 const app = express();
@@ -10,4 +11,13 @@ app.use('/api/movies',router);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}....`);
+    // don't save sensitive information such as password on json files used by config
+    /*
+    for windows pc use SET to assign NODE_ENV variable
+    for mac os/linux use export to assign it
+
+    set NODE_ENV=development
+    export NODE_ENV=production
+    */
+    console.log(`The details of db is ${config.get("db.host")} and of mail is ${config.get("mail.host")}`);
 });
